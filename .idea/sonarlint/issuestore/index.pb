f01d9b5c48ff2d912ae1a30b537b3c4e4278da35
Jack Zhang <zhangmengyu10@gmail.com>
2018-11-11 19:30:35 -0800

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+x
+Happ/src/main/java/com/jackz314/classregistrationhelper/MainActivity.java,6\b\6be30af43d33553f4534aae8533bacd9192a963c
---
+++ app/src/main/java/com/jackz314/classregistrationhelper/ListRecyclerAdapter.java
@@ -0,0 +1,125 @@
+package com.jackz314.classregistrationhelper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListRecyclerAdapter extends RecyclerView.Adapter<ListRecyclerAdapter.ListRecyclerViewHolder> implements Filterable {
+
+   private List<String[]> list;
+   private List<String[]>  arrayListFiltered;
+
+    /**
+     * Creates new List Recycler Adapter
+     *
+     * @param arrayList list with each element being an array
+     *                  array pos 0: class number
+     *                  array pos 1: class description/title
+     *                  array pos 2: class crn number
+     *                  array pos 3: class available seats count
+     */
+    ListRecyclerAdapter(ArrayList<String[]> arrayList){
+       this.list = arrayList;
+       arrayListFiltered = arrayList;
+   }
+
+   @NonNull
+   @Override
+   public ListRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       //can override at any time to inflate different layouts
+       Context context = parent.getContext();
+       View view = LayoutInflater.from(context).inflate(R.layout.course_info_item, parent, false);
+       return new ListRecyclerViewHolder(view);
+   }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ListRecyclerViewHolder holder, int position) {
+        if(!arrayListFiltered.isEmpty() && arrayListFiltered.size() > position){
+            final String classNumber = arrayListFiltered.get(position)[0];
+            final String classDescription = arrayListFiltered.get(position)[1];
+            final String crnNumber = arrayListFiltered.get(position)[2];
+            final String availableSeats = arrayListFiltered.get(position)[3];
+            holder.classNumberTxt.setText(classNumber);
+            holder.classDescriptionTxt.setText(classDescription);
+            holder.classCrnTxt.setText(crnNumber);
+            holder.availableSeatsTxt.setText(availableSeats);
+        }
+    }
+
+   static class ListRecyclerViewHolder extends RecyclerView.ViewHolder{
+       TextView classNumberTxt, classDescriptionTxt, classCrnTxt, availableSeatsTxt;
+       ListRecyclerViewHolder(View itemView) {
+           super(itemView);
+           classNumberTxt = itemView.findViewById(R.id.class_number_txt);
+           classDescriptionTxt = itemView.findViewById(R.id.class_description_txt);
+           classCrnTxt = itemView.findViewById(R.id.crn_no_txt);
+           availableSeatsTxt = itemView.findViewById(R.id.avaliable_seat_count_txt);
+       }
+   }
+
+   @Override
+   public int getItemCount() {
+       return list.size();
+   }
+
+   public int getFilteredItemCount(){
+        return arrayListFiltered.size();
+   }
+
+   @Override
+   public Filter getFilter(){
+        //System.out.println("initial count"+ getItemCount());
+       return new Filter() {
+           @Override
+           protected FilterResults performFiltering(CharSequence constraint) {
+               String query = constraint.toString();
+               List<String[]> arrTemp = new ArrayList<>();
+               if (query.isEmpty()) {
+                   arrTemp = list;
+               } else {
+                   //arrayListFiltered.clear();//clear previous stuff in it
+                   for (String[] classInfo : list) {
+                       //matching...
+                       if (classInfo[0].contains(query) || //number
+                               classInfo[2].contains(query) || //crn
+                               classInfo[1].toLowerCase().contains(query.toLowerCase())) { //description
+                           arrTemp.add(classInfo);
+                       }
+                   }
+               }
+               FilterResults filterResults = new FilterResults();
+               filterResults.values = arrTemp;
+               return filterResults;
+           }
+
+           @Override
+           protected void publishResults(CharSequence constraint, FilterResults results) {
+               arrayListFiltered = (List<String[]>) results.values;//it's fine, ignore the warning, I had to it this way, and the way that make sure the warning goes away slows down the performance, so no... maybe find out more about it in the future
+               System.out.println(" Filtered Result Size: "+arrayListFiltered.size() + " Original Size: " + list.size());
+               notifyDataSetChanged();
+           }
+       };
+   }
+
+   public String[] getItemAtPos(int position){
+       return arrayListFiltered.get(position);
+   }
+
+   public void swapNewDataSet(List<String[]> newList)
+   {
+       if(newList == null || newList.isEmpty()) return;
+       if (list != null && list.size()>0) list.clear();
+       list = newList;
+       notifyDataSetChanged();
+   }
+
+}
